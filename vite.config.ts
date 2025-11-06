5c725559c0000c904bc1c2b160581d5539df64ec
violetauko <aukoviolet24@gmail.com>
2025-11-06 09:13:09 +0300

    removed public

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,11 @@ export default defineConfig({
   build: {
   outDir: path.resolve(import.meta.dirname, "dist"),
   emptyOutDir: true,
+  rollupOptions: {
+    input: path.resolve(import.meta.dirname, "client", "index.html"),
+  },
 },
+
 
   server: {
     port: 3000,
