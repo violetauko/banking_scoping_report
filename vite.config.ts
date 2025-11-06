d9fc0358470ddf70157ec5b3a1ac2e1ab249d630
violetauko <aukoviolet24@gmail.com>
2025-11-06 10:04:11 +0300

    added server file

--- vite.config.ts
+++ vite.config.ts
@@ -20,14 +20,9 @@ export default defineConfig({
   envDir: path.resolve(import.meta.dirname),
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-  outDir: path.resolve(import.meta.dirname, "dist"),
-  emptyOutDir: true,
-  rollupOptions: {
-    input: path.resolve(import.meta.dirname, "client", "index.html"),
+    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    emptyOutDir: true,
   },
-},
-
-
   server: {
     port: 3000,
     strictPort: false, // Will find next available port if 3000 is busy
